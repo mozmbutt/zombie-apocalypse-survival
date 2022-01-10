7349e1ea82d32062935cacee88c3f9ecfc885598
devmozmbutt <moazzam.ali@devsinc.com>
2022-01-10 15:12:27 +0500

    bug fixes

--- app/controllers/infections_controller.rb
+++ app/controllers/infections_controller.rb
@@ -1,25 +1,20 @@
 class InfectionsController < ApplicationController
   before_action :set_infection, only: %i[ show edit update destroy ]
 
-  # GET /infections or /infections.json
   def index
     @infections = Infection.all
   end
 
-  # GET /infections/1 or /infections/1.json
   def show
   end
 
-  # GET /infections/new
   def new
     @infection = Infection.new
   end
 
-  # GET /infections/1/edit
   def edit
   end
 
-  # POST /infections or /infections.json
   def create
     @infection = Infection.new(infection_params)
 
@@ -34,7 +29,6 @@ class InfectionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /infections/1 or /infections/1.json
   def update
     respond_to do |format|
       if @infection.update(infection_params)
@@ -47,7 +41,6 @@ class InfectionsController < ApplicationController
     end
   end
 
-  # DELETE /infections/1 or /infections/1.json
   def destroy
     @infection.destroy
 
--- app/models/infection.rb
+++ app/models/infection.rb
@@ -1,3 +1,3 @@
 class Infection < ApplicationRecord
-    belongs_to :user
+	belongs_to :user
 end
