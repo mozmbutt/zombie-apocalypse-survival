173315048c84d82753f2f186302e6abf8e28b13d
devmozmbutt <moazzam.ali@devsinc.com>
2022-01-17 19:37:02 +0500

    location callback update

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,10 +4,10 @@ class Location < ApplicationRecord
   validates :lat, :lng, presence: true, numericality: { only_float: true }
 
   belongs_to :user
-  after_commit :update_previous_location, on: :create
+  before_create :update_previous_location
 
   def update_previous_location
-    user_last_location = user.locations.second_to_last
+    user_last_location = user.locations.last
     user_last_location.update!(current: false) unless user_last_location&.nil?
   end
 end
